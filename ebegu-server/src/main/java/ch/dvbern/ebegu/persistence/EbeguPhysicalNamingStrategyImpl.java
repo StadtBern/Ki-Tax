eaa0285b4235b1e086ac6dee3bf470c0902f21f7
Imanol Bernabeu <imanol.bernabeu@dvbern.ch>
2016-03-04 15:24:19 +0100

    EBEGU-198 hibernate5 implementiert NamingStrategy auf eine andere Art. Jetzt gibt es Physical und Implicit strategies aber keine von Ihnen implementierte Klasse folgt derselben Strategy wie früher (CamelCase -> underscore). Deshalb muss man eine neue klasse EbeguPhysicalNamingStrategyImpl implementieren. PROBLEM: hibernate4-maven-plugin geht nicht mit hibernate5 und braucht deshalb die alte konfiguration, die zwar im pom.xml definiert ist. Das Problem ist, dass plugin und Projekt haben 2 unterschiedlichen Klassen für dasselbe, obwohl die Implementierung gleich ist

---
+++ ebegu-server/src/main/java/ch/dvbern/ebegu/persistence/EbeguPhysicalNamingStrategyImpl.java
@@ -0,0 +1,41 @@
+package ch.dvbern.ebegu.persistence;
+
+import org.hibernate.boot.model.naming.Identifier;
+import org.hibernate.boot.model.naming.PhysicalNamingStrategyStandardImpl;
+import org.hibernate.engine.jdbc.env.spi.JdbcEnvironment;
+
+import java.io.Serializable;
+import java.util.Locale;
+
+/**
+ * Created by imanol on 04.03.16.
+ * In hibernate 5 die Strategies werden nicht mehr von NamingStrategy implementiert.
+ * Aus diesem Grund muss man seine eigene PhysicalStrategy bauen.
+ * Check http://stackoverflow.com/questions/32437202/improvednamingstrategy-no-longer-working-in-hibernate-5
+ */
+public class EbeguPhysicalNamingStrategyImpl extends PhysicalNamingStrategyStandardImpl implements Serializable {
+
+	public static final EbeguPhysicalNamingStrategyImpl INSTANCE = new EbeguPhysicalNamingStrategyImpl();
+
+	@Override
+	public Identifier toPhysicalTableName(Identifier name, JdbcEnvironment context) {
+		return new Identifier(addUnderscores(name.getText()), name.isQuoted());
+	}
+
+	@Override
+	public Identifier toPhysicalColumnName(Identifier name, JdbcEnvironment context) {
+		return new Identifier(addUnderscores(name.getText()), name.isQuoted());
+	}
+
+
+	protected static String addUnderscores(String name) {
+		final StringBuilder buf = new StringBuilder(name.replace('.', '_'));
+		for (int i = 1; i < buf.length() - 1; i++) {
+			if (Character.isLowerCase(buf.charAt(i - 1)) && Character.isUpperCase(buf.charAt(i))
+					&& Character.isLowerCase(buf.charAt(i + 1))) {
+				buf.insert(i++, '_');
+			}
+		}
+		return buf.toString().toLowerCase(Locale.ROOT);
+	}
+}
