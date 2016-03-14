b8f32bcbb4f0d61e89da337b19b646c22ce7732c
Imanol Bernabeu <imanol.bernabeu@dvbern.ch>
2016-03-14 10:08:12 +0100

    EBEGU-212 Konfigurationdateien fuer Typescript eingefuegt: typings und Grunt-Konfiguration

--- ebegu-web/Gruntfile.js
+++ ebegu-web/Gruntfile.js
@@ -290,13 +290,27 @@ module.exports = function (grunt) {
                     destination: 'build/doc'
                 }
             }
+        },
+        typescript: {
+            base: {
+                src: ['src/**/*.ts', '!**/*-spec.ts'],
+                dest: 'temp/src.js',
+                options: {
+                    module: 'amd', //or commonjs
+                    target: 'es5', //or es3
+                    //rootDir: '',
+                    sourceMap: true,
+                    declaration: true
+                }
+            }
         }
     });
 
     grunt.registerTask('build', ['clean:before', 'jshint:main', 'jscs:main', 'less:production', 'dom_munger', 'ngtemplates', 'cssmin', 'concat', 'ngAnnotate', 'uglify', 'copy', 'htmlmin', 'clean:after']);
-    grunt.registerTask('serve', ['dom_munger:read', 'jshint:main', 'jscs:main', 'configureProxies:dev', 'connect:dev', 'less:development', 'watch']);
+    grunt.registerTask('serve', ['dom_munger:read', 'jshint:main', 'jscs:main', 'configureProxies:dev', 'connect:dev', 'typescript', 'less:development', 'watch']);
     grunt.registerTask('test', ['dom_munger:read', 'karma:all_tests']);
     grunt.registerTask('doc', ['jsdoc']);
+    grunt.registerTask('ts', ['typescript']);
     grunt.registerTask('jenkins-build', ['clean:before', 'jshint:jenkins', 'jscs:jenkins', 'less:production', 'dom_munger:read', 'karma:jenkins', 'dom_munger', 'ngtemplates', 'cssmin', 'concat', 'ngAnnotate', 'uglify', 'copy', 'htmlmin', 'clean:after']);
     grunt.registerTask('jenkins-deploy-snapshot', ['jenkins-build', 'maven_deploy:snapshot_dist']);
     grunt.registerTask('jenkins-deploy-release', ['jenkins-build', 'maven_deploy:release_dist']);
---
+++ ebegu-web/typings/browser.d.ts
@@ -0,0 +1,5 @@
+/// <reference path="browser/ambient/angular-mocks/index.d.ts" />
+/// <reference path="browser/ambient/angular-resource/index.d.ts" />
+/// <reference path="browser/ambient/angular/index.d.ts" />
+/// <reference path="browser/ambient/jasmine/index.d.ts" />
+/// <reference path="browser/ambient/jquery/index.d.ts" />
