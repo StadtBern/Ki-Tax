5de4fb133fff9cc2450d84da3cf15f76a4e30293
Markus Hofstetter <markus.hofstetter@dvbern.ch>
2016-03-22 11:02:37 +0100

    direct2dev stammdaten

--- ebegu-web/src/core/core.route.ts
+++ ebegu-web/src/core/core.route.ts
@@ -5,8 +5,10 @@ module ebeguWeb.routes {
     angular.module('ebeguWeb.core').run(appRun);
 
     /* @ngInject */
-    export function appRun(routerHelper : IRouterHelper) {
+    export function appRun(angularMomentConfig : any ,routerHelper : IRouterHelper) {
         routerHelper.configureStates(getStates());
+        angularMomentConfig.format = 'DD.MM.YYYY'
+
     }
 
     function getStates() {
