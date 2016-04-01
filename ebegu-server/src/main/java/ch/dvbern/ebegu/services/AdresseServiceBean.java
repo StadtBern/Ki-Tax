a021b06f83cfdd2610877d094afd80079919bc22
Markus Hofstetter <markus.hofstetter@dvbern.ch>
2016-04-01 09:46:14 +0200

    EBEGU-225 Datenbank auf MARIA DB umstellen

--- ebegu-server/src/main/java/ch/dvbern/ebegu/services/AdresseServiceBean.java
+++ ebegu-server/src/main/java/ch/dvbern/ebegu/services/AdresseServiceBean.java
@@ -65,4 +65,103 @@ public class AdresseServiceBean extends AbstractBaseService implements AdresseSe
 		propertyToRemove.orElseThrow(() -> new EbeguEntityNotFoundException("removeAdresse", ErrorCodeEnum.ERROR_ENTITY_NOT_FOUND, adresse));
 		persistence.remove(propertyToRemove.get());
 	}
+
+	@Nonnull
+	@Override
+	public Optional<Adresse> getNewestWohnadresse(String personID) {
+		TypedQuery<Adresse> query = getAdresseQuery(personID, AdresseTyp.WOHNADRESSE, null, Constants.END_OF_TIME);
+		List<Adresse> results = query.getResultList();
+		//wir erwarten entweder keine oder genau eine Wohnadr, fuer eine Person mit guelitBis EndOfTime
+		if (results.isEmpty()) {
+			return Optional.empty();
+		}
+		if (results.size() > 1) {
+			throw new EbeguRuntimeException("getNewestWohnadresse", ErrorCodeEnum.ERROR_TOO_MANY_RESULTS, personID);
+		}
+		return Optional.of(results.get(0));
+
+	}
+
+	@Nonnull
+	@Override
+	public Adresse getCurrentWohnadresse(String personID) {
+		LocalDate today = LocalDate.now();
+		TypedQuery<Adresse> query = getAdresseQuery(personID, AdresseTyp.WOHNADRESSE, today, today);
+		List<Adresse> results = query.getResultList();
+		//wir erwarten entweder keine oder genau eine Wohnadr, fuer eine Person mit guelitBis EndOfTime
+		if (results.isEmpty()) {
+			throw new EbeguRuntimeException("getCurrentWohnaddresse", ErrorCodeEnum.ERROR_ENTITY_NOT_FOUND, personID);
+		}
+		if (results.size() > 1) {
+			throw new EbeguRuntimeException("getCurrentWohnaddresse", ErrorCodeEnum.ERROR_TOO_MANY_RESULTS, personID);
+		}
+		return results.get(0);
+
+	}
+
+	/**
+	 * Erstellt ein query gegen die Adresse mit den gegebenen parametern
+	 * @param personID person fuer die Adressen gesucht werden
+	 * @param typ typ der Adresse der gesucht wird
+	 * @param datumVon datum ab dem gesucht wird (incl)
+	 * @param datumBis datum bis zu dem gesucht wird (incl)
+	 * @return
+	 */
+	private TypedQuery<Adresse> getAdresseQuery(@Nonnull String personID, @Nonnull AdresseTyp typ, @Nullable LocalDate datumVon, @Nullable LocalDate datumBis) {
+		CriteriaBuilder cb = persistence.getCriteriaBuilder();
+		ParameterExpression<String> personIdParam = cb.parameter(String.class, "personID");
+		ParameterExpression<AdresseTyp> typParam = cb.parameter(AdresseTyp.class, "adresseTyp");
+		ParameterExpression<LocalDate> gueltigVonParam = cb.parameter(LocalDate.class, "gueltigVon");
+		ParameterExpression<LocalDate> gueltigBisParam = cb.parameter(LocalDate.class, "gueltigBis");
+
+		CriteriaQuery<Adresse> query = cb.createQuery(Adresse.class);
+		Root<Adresse> root = query.from(Adresse.class);
+		Predicate personPredicate = cb.equal(root.get(Adresse_.person).get(Person_.id), kindIdParam);
+		Predicate typePredicate = cb.equal(root.get(Adresse_.adresseTyp), typParam);
+		List<Expression<Boolean>> predicatesToUse = new ArrayList<>();
+
+		predicatesToUse.add(personPredicate);
+		predicatesToUse.add(typePredicate);
+		//noinspection VariableNotUsedInsideIf
+		if (datumVon != null) {
+			Predicate datumVonPredicate = cb.greaterThanOrEqualTo(root.get(gueltigAb), gueltigVonParam);
+			predicatesToUse.add(datumVonPredicate);
+
+		}
+		//noinspection VariableNotUsedInsideIf
+		if (datumBis != null) {
+			Predicate datumBisPredicate = cb.lessThanOrEqualTo(root.get(gueltigBis), gueltigBisParam);
+			predicatesToUse.add(datumBisPredicate);
+
+		}
+
+
+		query.where(criteriaQueryHelper.concatenateExpressions(cb, predicatesToUse));
+
+		TypedQuery<Adresse> typedQuery = persistence.getEntityManager().createQuery(query);
+
+		typedQuery.setParameter("personID", personID);
+		typedQuery.setParameter("adresseTyp", typ);
+		if (datumVon != null) {
+			typedQuery.setParameter("gueltigVon", datumVon);
+		}
+		if (datumBis != null) {
+			typedQuery.setParameter("gueltigBis", datumBis);
+		}
+		return typedQuery;
+	}
+
+	@Nonnull
+	@Override
+	public Optional<Adresse> getKorrespondenzAdr(String personID) {
+
+		List<Adresse> results = getAdresseQuery(personID, AdresseTyp.KORRESPONDENZADRESSE, null, null).getResultList();
+		if (results.isEmpty()) {
+			return Optional.empty();
+		}
+		if (results.size() > 1) {
+			throw new EbeguRuntimeException("getKorrespondenzAdr", ErrorCodeEnum.ERROR_TOO_MANY_RESULTS, personID);
+		}
+		return Optional.of(results.get(0));
+	}
 }
