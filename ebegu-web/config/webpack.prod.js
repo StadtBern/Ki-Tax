b9faf6d4c95c7871fcea99729ac9e04d20d4604d
Markus Hofstetter <markus.hofstetter@dvbern.ch>
2016-04-13 19:12:58 +0200

    EBEGU-226 ports und server fuer demo freigeben

--- ebegu-web/config/webpack.prod.js
+++ ebegu-web/config/webpack.prod.js
@@ -23,7 +23,7 @@ var WebpackMd5Hash = require('webpack-md5-hash');
  */
 const ENV = process.env.NODE_ENV = process.env.ENV = 'production';
 const HOST = process.env.HOST || 'localhost';
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 80;
 const METADATA = webpackMerge(commonConfig.metadata, {
     host: HOST,
     port: PORT,
