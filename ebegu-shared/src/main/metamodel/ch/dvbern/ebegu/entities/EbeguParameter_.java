56233cd4abbbe74c6874035524345399772dfd64
Markus Hofstetter <markus.hofstetter@dvbern.ch>
2016-06-14 12:21:15 +0200

    Aus leider unbekannten Gruenden verlieren wir in der IDE (inteliJ) zwischendurch ploetzlich Attribute der Metamodelklassen.
    Zum Beispiel geht die Adresse des Gesuchstellers_.adresse verloren. Die Klasse Gesuchsteller_ ist dann zwar noch da aber die Attribute sind weg.
    
    Damit wir hier nicht immer neu generieren muessen checken wir das Metamodel mal komplett ein.

---
+++ ebegu-shared/src/main/metamodel/ch/dvbern/ebegu/entities/EbeguParameter_.java
@@ -0,0 +1,16 @@
+package ch.dvbern.ebegu.entities;
+
+import ch.dvbern.ebegu.enums.EbeguParameterKey;
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value = "org.hibernate.jpamodelgen.JPAMetaModelEntityProcessor")
+@StaticMetamodel(EbeguParameter.class)
+public abstract class EbeguParameter_ extends ch.dvbern.ebegu.entities.AbstractDateRangedEntity_ {
+
+	public static volatile SingularAttribute<EbeguParameter, EbeguParameterKey> name;
+	public static volatile SingularAttribute<EbeguParameter, String> value;
+
+}
+
