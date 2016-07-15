e2b9d3f3000eb9e91eea59ab1209fca099ba4c04
Markus Hofstetter <markus.hofstetter@dvbern.ch>
2016-07-15 15:04:43 +0200

    EBEGU-301 Bei der Erfassung neuen Entitäten, z.B. Kinder kann ich eine Entität durch schnelles Klicken auf "hinzufügen" zwei Mal erstellen
    >  der dv-loading-button kann jetzt disabled werden mittels ng-disabled. Dafuer muss aber ein allfaelliges Click event in button-click gewechselt werden
    
    > alle buttons ausgetauscht

--- ebegu-web/src/core/directive/dv-loading-button/dv-loading-button.ts
+++ ebegu-web/src/core/directive/dv-loading-button/dv-loading-button.ts
@@ -5,24 +5,31 @@ import INgModelController = angular.INgModelController;
 import IHttpService = angular.IHttpService;
 import ITimeoutService = angular.ITimeoutService;
 import IFormController = angular.IFormController;
+import IScope = angular.IScope;
+import IAugmentedJQuery = angular.IAugmentedJQuery;
+import IAttributes = angular.IAttributes;
+import IAngularEvent = angular.IAngularEvent;
 let template = require('./dv-loading-button.html');
 
 
 interface IDVLoadingButtonController {
-    isLoading: () => {};
+    isDisabled: boolean;
     buttonDisabled: boolean;
 }
 
 export class DVLoadingButton implements IDirective {
     transclude = true;
     restrict = 'E';
-    require: any = {formCtrl: '^?form'};
+    require: any = {dvLoadingButtonCtrl: 'dvLoadingButton', formCtrl: '^?form'};
     replace = true;
     scope = {
         type: '@',
         delay: '@',
         buttonClass: '@',
-        forceWaitService: '@'
+        forceWaitService: '@',
+        buttonDisabled: '<',
+        buttonClick: '&'
+
     };
     template = template;
     controller = DVLoadingButtonController;
@@ -41,21 +48,39 @@ export class DVLoadingButton implements IDirective {
  * the form is valid first. If not it will not disable itself.
  * By default the button will be disabled till the next REST servicecall returns (not neceserally the one that was triggered
  * by this button) or till 400 ms have expired
+* @example:
+ * 
+    <dv-loading-button type="submit" 
+         button-click="vm.mySaveFunction(form)" 
+         button-class="btn btn-sm btn-success"
+         button-disabled="!vm.isButtonDisabled()">
+             <i class="glyphicon glyphicon-plus"></i>
+             <span data-translate="SAVE"></span>
+      </dv-loading-button>
+
+ *     
  */
 export class DVLoadingButtonController implements IDVLoadingButtonController {
     static $inject: string[] = ['$http', '$scope', '$timeout'];
 
-    isLoading: () => boolean;
     buttonClicked: () => void;
-    buttonDisabled: boolean;
+    isDisabled: boolean;
     formCtrl: IFormController;
     delay: string;
     forceWaitService: string;
+    buttonDisabled: boolean; //true wenn unser element programmatisch disabled wird
+    buttonClick: () => void;
 
     /* @ngInject */
     constructor(private $http: IHttpService, private $scope: any, private $timeout: ITimeoutService) {
 
         this.buttonClicked = () => {
+            //wenn der button diesabled ist machen wir mal gar nichts
+            if (this.buttonDisabled || this.isDisabled) {
+                return;
+            }
+            this.buttonClick();  //falls ein button-click callback uebergeben wurde ausfuehren
+
             //timeout wird gebraucht damit der request nach dem disablen ueberhaupt uebermittelt wird
             $timeout(() => {
                 if (!this.forceWaitService) {
@@ -65,21 +90,32 @@ export class DVLoadingButtonController implements IDVLoadingButtonController {
                             this.disableForDelay();
                         }
                     } else { //wenn kein form einfach mal disablen fuer delay ms
+
                         this.disableForDelay();
                     }
                 } else {
                     //wir warten auf naechsten service return, egal wie lange es dauert
-                    this.buttonDisabled = true;
+                    this.isDisabled = true;
                 }
             }, 0);
 
         };
 
         this.$scope.$on(TSHTTPEvent.REQUEST_FINISHED, (event: any) => {
-            this.buttonDisabled = false;
+            this.isDisabled = false;
         });
 
     }
+
+
+    // beispiel wie man auf changes eines attributes von aussen reagieren kann
+    $onChanges(changes: any) {
+        if (changes.buttonDisabled && !changes.buttonDisabled.isFirstChange()) {
+            this.buttonDisabled = changes.buttonDisabled.currentValue;
+        }
+
+    }
+
 
     private getDelay(): number {
         if (this.delay) {
@@ -95,9 +131,9 @@ export class DVLoadingButtonController implements IDVLoadingButtonController {
      * disabled den Button fuer "delay" milisekunden
      */
     private disableForDelay(): void {
-        this.buttonDisabled = true;
+        this.isDisabled = true;
         this.$timeout(() => {
-            this.buttonDisabled = false;
+            this.isDisabled = false;
         }, this.getDelay());
 
     }
