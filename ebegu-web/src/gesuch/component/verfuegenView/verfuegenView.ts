07fb5d116c42f160b58c19a6bcc47a1ea86d1381
Imanol Bernabeu <imanol.bernabeu@dvbern.ch>
2016-07-27 16:13:52 +0200

    EBEGU-260b - Abzuege im Layout von Verfuegen eingebaut. Auch die Parameter wie familiengroesse oder Anfangsperiode werden berechnet und angezeigt

--- ebegu-web/src/gesuch/component/verfuegenView/verfuegenView.ts
+++ ebegu-web/src/gesuch/component/verfuegenView/verfuegenView.ts
@@ -4,6 +4,8 @@ import GesuchModelManager from '../../service/gesuchModelManager';
 import {IStateService} from 'angular-ui-router';
 import EbeguUtil from '../../../utils/EbeguUtil';
 import {TSBetreuungsstatus} from '../../../models/enums/TSBetreuungsstatus';
+import BerechnungsManager from '../../service/berechnungsManager';
+import DateUtil from '../../../utils/DateUtil';
 let template = require('./verfuegenView.html');
 require('./verfuegenView.less');
 
@@ -17,18 +19,37 @@ export class VerfuegenViewComponentConfig implements IComponentOptions {
 
 export class VerfuegenViewController extends AbstractGesuchViewController {
 
-    static $inject: string[] = ['$state', 'GesuchModelManager', 'EbeguUtil'];
+    static $inject: string[] = ['$state', 'GesuchModelManager', 'BerechnungsManager', 'EbeguUtil'];
 
     private nombres: string[] = ['hola', 'adios'];
 
     /* @ngInject */
-    constructor(state: IStateService, gesuchModelManager: GesuchModelManager, private ebeguUtil: EbeguUtil) {
-        super(state, gesuchModelManager, undefined);
-        this.initViewModel();
+    constructor(state: IStateService, gesuchModelManager: GesuchModelManager, berechnungsManager: BerechnungsManager,
+                private ebeguUtil: EbeguUtil) {
+        super(state, gesuchModelManager, berechnungsManager);
+        this.initModel();
     }
 
-    private initViewModel(): void {
+    /**
+     * Die finanzielle Situation und die Einkommensverschlechterungen muessen mithilfe des Berechnungsmanagers berechnet werden, um manche Daten zur Verfügung
+     * zu haben. Das ist notwendig weil die finanzielle Situation nicht gespeichert wird. D.H. das erste Mal in einer Sitzung wenn ein Gesuch geoeffnet wird,
+     * ist gar nichts berechnet. Wenn man dann die Verfügen direkt aufmacht, ist alles leer und wird nichts angezeigt, deswegen muss alles auch hier berechnet werden.
+     * Um Probleme mit der Performance zu vermeiden, wird zuerst geprueft, ob die Berechnung schon vorher gemacht wurde, wenn ja dann wird sie einfach verwendet
+     * ohne sie neu berechnen zu muessen. Dieses geht aber davon aus, dass die Berechnungen immer richtig kalkuliert wurden
+     */
+    private initModel(): void {
+        console.log('finSit -> cargado');
+        this.berechnungsManager.calculateFinanzielleSituation(this.gesuchModelManager.gesuch); //.then(() => {});
+        if (this.gesuchModelManager.gesuch.einkommensverschlechterungInfo && this.gesuchModelManager.gesuch.einkommensverschlechterungInfo.ekvFuerBasisJahrPlus1) {
+            console.log('EV1 -> cargado');
+            this.berechnungsManager.calculateEinkommensverschlechterung(this.gesuchModelManager.gesuch, 1); //.then(() => {});
+        }
+        if (this.gesuchModelManager.gesuch.einkommensverschlechterungInfo && this.gesuchModelManager.gesuch.einkommensverschlechterungInfo.ekvFuerBasisJahrPlus2) {
+            console.log('EV2 -> cargado');
+            this.berechnungsManager.calculateEinkommensverschlechterung(this.gesuchModelManager.gesuch, 2); //.then(() => {});
+        }
     }
+
 
     public cancel(): void {
         this.state.go('gesuch.verfuegen');
@@ -82,6 +103,48 @@ export class VerfuegenViewController extends AbstractGesuchViewController {
     public getBetreuungsstatus(): TSBetreuungsstatus {
         if (this.gesuchModelManager && this.gesuchModelManager.getBetreuungToWorkWith()) {
             return this.gesuchModelManager.getBetreuungToWorkWith().betreuungsstatus;
+        }
+        return undefined;
+    }
+
+    public getAnfangsPeriode(): string {
+        if (this.ebeguUtil) {
+            return this.ebeguUtil.getFirstDayGesuchsperiodeAsString(this.gesuchModelManager.getGesuchsperiode());
+        }
+        return undefined;
+    }
+
+    public getFamiliengroesseFS(): number {
+        if (this.berechnungsManager && this.berechnungsManager.finanzielleSituationResultate) {
+            return this.berechnungsManager.finanzielleSituationResultate.familiengroesse;
+        }
+        return undefined;
+    }
+
+    public getFamiliengroesseEV1(): number {
+        if (this.berechnungsManager && this.berechnungsManager.einkommensverschlechterungResultateBjP1) {
+            return this.berechnungsManager.einkommensverschlechterungResultateBjP1.familiengroesse;
+        }
+        return undefined;
+    }
+
+    public getFamiliengroesseEV2(): number {
+        if (this.berechnungsManager && this.berechnungsManager.einkommensverschlechterungResultateBjP1) {
+            return this.berechnungsManager.einkommensverschlechterungResultateBjP2.familiengroesse;
+        }
+        return undefined;
+    }
+
+    public getAnfangsVerschlechterung1(): string {
+        if (this.gesuchModelManager && this.gesuchModelManager.gesuch && this.gesuchModelManager.gesuch.einkommensverschlechterungInfo) {
+            return DateUtil.momentToLocalDateFormat(this.gesuchModelManager.gesuch.einkommensverschlechterungInfo.stichtagFuerBasisJahrPlus1, 'DD.MM.YYYY');
+        }
+        return undefined;
+    }
+
+    public getAnfangsVerschlechterung2(): string {
+        if (this.gesuchModelManager && this.gesuchModelManager.gesuch && this.gesuchModelManager.gesuch.einkommensverschlechterungInfo) {
+            return DateUtil.momentToLocalDateFormat(this.gesuchModelManager.gesuch.einkommensverschlechterungInfo.stichtagFuerBasisJahrPlus2, 'DD.MM.YYYY');
         }
         return undefined;
     }
