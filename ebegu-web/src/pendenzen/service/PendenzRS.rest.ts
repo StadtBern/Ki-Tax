658f01564de7712c05c6201967cb439dfdc3016f
Markus Hofstetter <markus.hofstetter@dvbern.ch>
2016-07-27 16:17:11 +0200

    EBEGU-260 Maske Verfügungstabelle
    > Finanzielle Situation muss berechnet sein

--- ebegu-web/src/pendenzen/service/PendenzRS.rest.ts
+++ ebegu-web/src/pendenzen/service/PendenzRS.rest.ts
@@ -10,7 +10,7 @@ export default class PendenzRS {
 
     static $inject = ['$http', 'REST_API', 'EbeguRestUtil', '$log'];
     /* @ngInject */
-    constructor($http: IHttpService, REST_API: string, ebeguRestUtil: EbeguRestUtil, $log: ILogService) {
+    constructor($http: IHttpService, private REST_API: string, ebeguRestUtil: EbeguRestUtil, $log: ILogService) {
         this.serviceURL = REST_API + 'pendenzen';
         this.http = $http;
         this.ebeguRestUtil = ebeguRestUtil;
@@ -22,11 +22,20 @@ export default class PendenzRS {
     }
 
     public getPendenzenList(): IPromise<Array<TSPendenzJA>> {
+        this.dotest();
         return this.http.get(this.serviceURL)
             .then((response: any) => {
                 this.log.debug('PARSING pendenz REST object ', response.data);
                 return this.ebeguRestUtil.parsePendenzen(response.data);
             });
+    }
+
+    public dotest() {
+        console.log("testing mandant call")
+        return this.http.get(this.REST_API + 'verfuegung/calculate/cdef5c90-eac5-41eb-af68-907bbf94d665').then((response: any) => {
+            this.log.debug('PARSING verfuegung REST object ', response.data);
+
+        });
     }
 
 }
