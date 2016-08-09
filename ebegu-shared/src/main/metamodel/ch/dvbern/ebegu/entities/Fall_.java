ee894305c18a89f56e22fe31b59573dd70b62812
Pascal Gatschet <pascal.gatschet@dvbern.ch>
2016-08-09 08:59:02 +0200

    small changes

--- ebegu-shared/src/main/metamodel/ch/dvbern/ebegu/entities/Fall_.java
+++ ebegu-shared/src/main/metamodel/ch/dvbern/ebegu/entities/Fall_.java
@@ -1,12 +1,16 @@
 package ch.dvbern.ebegu.entities;
 
 import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
 @Generated(value = "org.hibernate.jpamodelgen.JPAMetaModelEntityProcessor")
 @StaticMetamodel(Fall.class)
 public abstract class Fall_ extends ch.dvbern.ebegu.entities.AbstractEntity_ {
 
+	public static volatile SingularAttribute<Fall, Integer> fallNummer;
+	public static volatile SingularAttribute<Fall, Integer> nextNumberKind;
+	public static volatile SingularAttribute<Fall, Benutzer> verantwortlicher;
 
 }
 
