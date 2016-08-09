54c86cea4070eb8118506d8d4f16f96450ec3e24
Imanol Bernabeu <imanol.bernabeu@dvbern.ch>
2016-08-09 10:41:31 +0200

    EBEGU-325 - Classes_

--- ebegu-shared/src/main/metamodel/ch/dvbern/ebegu/entities/Fachstelle_.java
+++ ebegu-shared/src/main/metamodel/ch/dvbern/ebegu/entities/Fachstelle_.java
@@ -1,12 +1,16 @@
 package ch.dvbern.ebegu.entities;
 
 import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
 @Generated(value = "org.hibernate.jpamodelgen.JPAMetaModelEntityProcessor")
 @StaticMetamodel(Fachstelle.class)
 public abstract class Fachstelle_ extends ch.dvbern.ebegu.entities.AbstractEntity_ {
 
+	public static volatile SingularAttribute<Fachstelle, String> name;
+	public static volatile SingularAttribute<Fachstelle, String> beschreibung;
+	public static volatile SingularAttribute<Fachstelle, Boolean> behinderungsbestaetigung;
 
 }
 
