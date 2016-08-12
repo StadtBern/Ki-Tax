a85b76920be31d85e56033039bd81f20162f8f72
Imanol Bernabeu <imanol.bernabeu@dvbern.ch>
2016-08-12 13:19:33 +0200

    EBEGU-325 - Menueelemente werden fuer Institution ausgeblendet. Nur Gesuchsteller, Betreuung und Verfuegung werden angezeigt. - Neue Klasse TSRoleUtil in der man Methode findet, die unterschiedlichen arrays mit Rollen zurueckgeben

---
+++ ebegu-web/src/utils/TSRoleUtil.ts
@@ -0,0 +1,23 @@
+import {TSRole} from '../models/enums/TSRole';
+/**
+ * Hier findet man unterschiedliche Hilfsmethoden, um die Rollen von TSRole zu holen
+ */
+export class TSRoleUtil {
+
+    static getAllRolesButGesuchsteller(): Array<string> {
+        return TSRoleUtil.getAllRoles().filter(element =>
+            element !== TSRole[TSRole.GESUCHSTELLER]
+        );
+    }
+
+    public static getAllRoles(): Array<string> {
+        // return Object.keys(TSRole).map(k => TSRole[k]);
+        let result: Array<string> = [];
+        for (var prop in TSRole) {
+            if ((isNaN(parseInt(prop)))) {
+                result.push(prop);
+            }
+        }
+        return result;
+    }
+}
