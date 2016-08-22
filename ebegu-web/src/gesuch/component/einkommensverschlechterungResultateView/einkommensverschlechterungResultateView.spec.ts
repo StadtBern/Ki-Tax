ff43c7b4ada438ebae91e7d15c5a6799a96bb411
Pascal Gatschet <pascal.gatschet@dvbern.ch>
2016-08-22 12:37:42 +0200

    Added new tests. Problem kann momentan nicht reproduziert werden!?

--- ebegu-web/src/gesuch/component/einkommensverschlechterungResultateView/einkommensverschlechterungResultateView.spec.ts
+++ ebegu-web/src/gesuch/component/einkommensverschlechterungResultateView/einkommensverschlechterungResultateView.spec.ts
@@ -3,19 +3,28 @@ import 'angular-mocks';
 import {EbeguWebGesuch} from '../../gesuch.module';
 import GesuchModelManager from '../../service/gesuchModelManager';
 import BerechnungsManager from '../../service/berechnungsManager';
+import {EinkommensverschlechterungResultateViewController} from './einkommensverschlechterungResultateView';
+import TSFinanzielleSituationResultateDTO from '../../../models/dto/TSFinanzielleSituationResultateDTO';
 import IInjectorService = angular.auto.IInjectorService;
 import IHttpBackendService = angular.IHttpBackendService;
+import IStateService = angular.ui.IStateService;
 
 describe('einkommensverschlechterungResultateView', function () {
 
     let gesuchModelManager: GesuchModelManager;
     let berechnungsManager: BerechnungsManager;
+    let ekvrvc: EinkommensverschlechterungResultateViewController;
+    let $state: IStateService;
 
     beforeEach(angular.mock.module(EbeguWebGesuch.name));
 
     var component: any;
     var scope: angular.IScope;
     var $componentController: any;
+    var stateParams: any;
+    var state: any;
+    var consta: any;
+    var errorservice: any;
 
     beforeEach(angular.mock.inject(function ($injector: any) {
         $componentController = $injector.get('$componentController');
@@ -23,6 +32,18 @@ describe('einkommensverschlechterungResultateView', function () {
         berechnungsManager = $injector.get('BerechnungsManager');
         let $rootScope = $injector.get('$rootScope');
         scope = $rootScope.$new();
+        let ebeguRestUtil = $injector.get('EbeguRestUtil');
+        let $q = $injector.get('$q');
+        stateParams = $injector.get('$stateParams');
+        state = $injector.get('$state');
+        consta = $injector.get('CONSTANTS');
+        errorservice = $injector.get('ErrorService');
+
+
+        spyOn(berechnungsManager, 'calculateFinanzielleSituation').and.returnValue($q.when({}));
+        // ekvrvc = new EinkommensverschlechterungResultateViewController($injector.get('$stateParams'), $injector.get('$state'), gesuchModelManager,
+        //     berechnungsManager, $injector.get('CONSTANTS'), $injector.get('ErrorService'));
+
     }));
 
     beforeEach(function () {
@@ -37,4 +58,50 @@ describe('einkommensverschlechterungResultateView', function () {
         component = $componentController('einkommensverschlechterungResultateView', {$scope: scope}, bindings);
         expect(component).toBeDefined();
     });
+
+    describe('calculateVeraenderung', () => {
+        beforeEach(function () {
+            //spyOn(berechnungsManager, 'calculateFinanzielleSituation').and.returnValue($q.when({}));
+            ekvrvc = new EinkommensverschlechterungResultateViewController(stateParams, state, gesuchModelManager,
+                berechnungsManager, consta, errorservice);
+
+        });
+        it('should return + 100.0%', () => {
+
+            setValues(100, 200);
+            expect(ekvrvc.calculateVeraenderung()).toEqual('+ 100.0 %');
+        });
+
+        it('should return - 50.0 %', () => {
+
+            setValues(200, 100);
+            expect(ekvrvc.calculateVeraenderung()).toEqual('- 50.0 %');
+        });
+
+        it('should return - 90.0%', () => {
+
+            setValues(200, 20);
+            expect(ekvrvc.calculateVeraenderung()).toEqual('- 90.0 %');
+        });
+
+        it('should return - 81.2 %', () => {
+
+            setValues(59720, 11230);
+            expect(ekvrvc.calculateVeraenderung()).toEqual('- 81.2 %');
+        });
+
+        function setValues( massgebendesEinkommen_vj: number, massgebendesEinkommen_bj: number) {
+            let finsint: TSFinanzielleSituationResultateDTO = new TSFinanzielleSituationResultateDTO();
+            finsint.massgebendesEinkommen = massgebendesEinkommen_bj;
+
+            let finsintvj: TSFinanzielleSituationResultateDTO = new TSFinanzielleSituationResultateDTO();
+            finsintvj.massgebendesEinkommen = massgebendesEinkommen_vj;
+
+            spyOn(ekvrvc, 'getResultate').and.returnValue(finsint);
+            ekvrvc.resultatVorjahr = finsintvj;
+        }
+
+    });
 });
+
+
