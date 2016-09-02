5f8420844528437f1249cb5dd9837324eb271c25
Imanol Bernabeu <imanol.bernabeu@dvbern.ch>
2016-09-02 15:29:37 +0200

    EBEGU-329 - Confirmation Dialog beim Wechseln in Familiensituation von 2GS auf 1GS

--- ebegu-web/src/gesuch/component/familiensituationView/familiensituationView.ts
+++ ebegu-web/src/gesuch/component/familiensituationView/familiensituationView.ts
@@ -16,8 +16,12 @@ import {TSRole} from '../../../models/enums/TSRole';
 import WizardStepManager from '../../service/wizardStepManager';
 import {TSWizardStepName} from '../../../models/enums/TSWizardStepName';
 import {TSWizardStepStatus} from '../../../models/enums/TSWizardStepStatus';
+import {DvDialog} from '../../../core/directive/dv-dialog/dv-dialog';
+import {RemoveDialogController} from '../../dialog/RemoveDialogController';
+import ITranslateService = angular.translate.ITranslateService;
 let template = require('./familiensituationView.html');
 require('./familiensituationView.less');
+let removeDialogTemplate = require('../../dialog/removeDialogTemplate.html');
 
 
 export class FamiliensituationViewComponentConfig implements IComponentOptions {
@@ -33,14 +37,19 @@ export class FamiliensituationViewController extends AbstractGesuchViewControlle
     familienstatusValues: Array<TSFamilienstatus>;
     gesuchstellerKardinalitaetValues: Array<TSGesuchstellerKardinalitaet>;
     allowedRoles: Array<TSRole>;
+    initialFamiliensituation: TSFamiliensituation;
 
-    static $inject = ['$state', 'GesuchModelManager', 'BerechnungsManager', 'ErrorService', 'WizardStepManager'];
+    static $inject = ['$state', 'GesuchModelManager', 'BerechnungsManager', 'ErrorService', 'WizardStepManager',
+                      'DvDialog', '$translate'];
     /* @ngInject */
     constructor($state: IStateService, gesuchModelManager: GesuchModelManager, berechnungsManager: BerechnungsManager,
-                private errorService: ErrorService, wizardStepManager: WizardStepManager) {
+                private errorService: ErrorService, wizardStepManager: WizardStepManager, private DvDialog: DvDialog,
+                private $translate: ITranslateService) {
+
         super($state, gesuchModelManager, berechnungsManager, wizardStepManager);
         this.familienstatusValues = getTSFamilienstatusValues();
         this.gesuchstellerKardinalitaetValues = getTSGesuchstellerKardinalitaetValues();
+        this.initialFamiliensituation = angular.copy(this.gesuchModelManager.getFamiliensituation());
         this.initViewModel();
     }
 
@@ -52,15 +61,31 @@ export class FamiliensituationViewController extends AbstractGesuchViewControlle
     }
 
     previousStep(form: IFormController): void {
-        this.save(form, (response: any) => {
+        this.confirmAndSave(form, (response: any) => {
             this.state.go('gesuch.fallcreation');
         });
     }
 
     nextStep(form: IFormController): void {
-        this.save(form, (response: any) => {
+        this.confirmAndSave(form, (response: any) => {
             this.state.go('gesuch.stammdaten');
         });
+    }
+
+    private confirmAndSave(form: angular.IFormController, navigationFunction: (gesuch: any) => any) {
+        if (this.isConfirmationRequired()) {
+            let descriptionText: any = this.$translate.instant('FAMILIENSITUATION_WARNING_BESCHREIIBUNG', {
+                gsfullname: this.gesuchModelManager.getGesuch().gesuchsteller2 ? this.gesuchModelManager.getGesuch().gesuchsteller2.getFullName() : ''
+            });
+            this.DvDialog.showDialog(removeDialogTemplate, RemoveDialogController, {
+                title: 'FAMILIENSITUATION_WARNING',
+                deleteText: descriptionText
+            }).then(() => {   //User confirmed changes
+                this.save(form, navigationFunction);
+            });
+        } else {
+            this.save(form, navigationFunction);
+        }
     }
 
     private save(form: angular.IFormController, navigationFunction: (gesuch: any) => any) {
@@ -79,4 +104,13 @@ export class FamiliensituationViewController extends AbstractGesuchViewControlle
         return this.gesuchModelManager.getFamiliensituation();
     }
 
+    /**
+     * Confirmation is required when the GS2 already exists and the familiensituation changes from 2GS to 2GS
+     * @returns {boolean}
+     */
+    private isConfirmationRequired(): boolean {
+        return (this.gesuchModelManager.getGesuch().gesuchsteller2 && this.gesuchModelManager.getGesuch().gesuchsteller2.id
+                && this.initialFamiliensituation.hasSecondGesuchsteller()
+                && !this.gesuchModelManager.getFamiliensituation().hasSecondGesuchsteller());
+    }
 }
