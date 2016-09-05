452c41a5f5b1d56a2bc02b6bc7004c3dc7f1b90f
Imanol Bernabeu <imanol.bernabeu@dvbern.ch>
2016-09-05 14:23:22 +0200

    EBEGU-329 - sql script geflickt

--- ebegu-dbschema/src/main/resources/db/migration/V0028__fieldVerfuegbarWizardStep.sql
+++ ebegu-dbschema/src/main/resources/db/migration/V0028__fieldVerfuegbarWizardStep.sql
@@ -5,5 +5,5 @@ ALTER TABLE wizard_step_aud
   ADD COLUMN verfuegbar BIT;
 
 # Alle alte Gesuche aktualisieren
-UPDATE wizard_step SET verfuegbar=false where wizard_step_name = 'UNBESUCHT';
-UPDATE wizard_step SET verfuegbar=true where wizard_step_name <> 'UNBESUCHT';
+UPDATE wizard_step SET verfuegbar=false where wizard_step_status = 'UNBESUCHT';
+UPDATE wizard_step SET verfuegbar=true where wizard_step_status <> 'UNBESUCHT';
