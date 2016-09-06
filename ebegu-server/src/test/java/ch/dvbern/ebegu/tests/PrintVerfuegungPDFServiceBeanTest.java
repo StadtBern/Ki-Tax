8a0c83603e6bd3acefd74f79b009fa2e0752d455
Markus Hofstetter <markus.hofstetter@dvbern.ch>
2016-09-06 16:19:37 +0200

    EBEGU-458  Verfügungs PDF-erstellen - Finalisieren     tests sollten nicht direkt file oeffnen

--- ebegu-server/src/test/java/ch/dvbern/ebegu/tests/PrintVerfuegungPDFServiceBeanTest.java
+++ ebegu-server/src/test/java/ch/dvbern/ebegu/tests/PrintVerfuegungPDFServiceBeanTest.java
@@ -1,14 +1,13 @@
 package ch.dvbern.ebegu.tests;
 
-import java.awt.Desktop;
-import java.io.File;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Locale;
-
-import javax.inject.Inject;
-
+import ch.dvbern.ebegu.entities.Gesuch;
+import ch.dvbern.ebegu.entities.InstitutionStammdaten;
+import ch.dvbern.ebegu.rechner.AbstractBGRechnerTest;
+import ch.dvbern.ebegu.rules.BetreuungsgutscheinEvaluator;
+import ch.dvbern.ebegu.services.GesuchService;
+import ch.dvbern.ebegu.services.PrintVerfuegungPDFService;
+import ch.dvbern.ebegu.testfaelle.Testfall01_WaeltiDagmar;
+import ch.dvbern.ebegu.tets.TestDataUtil;
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.arquillian.persistence.UsingDataSet;
@@ -20,14 +19,13 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import ch.dvbern.ebegu.entities.Gesuch;
-import ch.dvbern.ebegu.entities.InstitutionStammdaten;
-import ch.dvbern.ebegu.rechner.AbstractBGRechnerTest;
-import ch.dvbern.ebegu.rules.BetreuungsgutscheinEvaluator;
-import ch.dvbern.ebegu.services.GesuchService;
-import ch.dvbern.ebegu.services.PrintVerfuegungPDFService;
-import ch.dvbern.ebegu.testfaelle.Testfall01_WaeltiDagmar;
-import ch.dvbern.ebegu.tets.TestDataUtil;
+import javax.inject.Inject;
+import java.awt.*;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
 
 /**
  * Test der die vom JA gemeldeten Testfaelle ueberprueft.
@@ -78,7 +76,7 @@ public class PrintVerfuegungPDFServiceBeanTest extends AbstractEbeguTest {
 		for (byte[] verfDoc : verfuegungsPDFs) {
 			Assert.assertNotNull(verfDoc);
 			File file = writeToTempDir(verfDoc, "TN_Verfuegung" + i + ".pdf");
-			openPDF(file);
+//			openPDF(file);
 			i++;
 		}
 
