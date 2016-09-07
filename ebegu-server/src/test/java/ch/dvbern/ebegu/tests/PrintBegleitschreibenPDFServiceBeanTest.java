40180e831a5af4e63b4687db5ed43e5ddeec2db4
Abdesselam Zerrouka <abdesselam.zerrouka@dvbern.ch>
2016-09-07 07:46:35 +0200

    EBEGU-458-2   Verfügungs PDF-erstellen - Finalisieren

--- ebegu-server/src/test/java/ch/dvbern/ebegu/tests/PrintBegleitschreibenPDFServiceBeanTest.java
+++ ebegu-server/src/test/java/ch/dvbern/ebegu/tests/PrintBegleitschreibenPDFServiceBeanTest.java
@@ -1,14 +1,13 @@
 package ch.dvbern.ebegu.tests;
 
-import ch.dvbern.ebegu.entities.Gesuch;
-import ch.dvbern.ebegu.entities.InstitutionStammdaten;
-import ch.dvbern.ebegu.rechner.AbstractBGRechnerTest;
-import ch.dvbern.ebegu.rules.BetreuungsgutscheinEvaluator;
-import ch.dvbern.ebegu.services.GesuchService;
-import ch.dvbern.ebegu.services.PrintBegleitschreibenPDFService;
-import ch.dvbern.ebegu.testfaelle.Testfall01_WaeltiDagmar;
-import ch.dvbern.ebegu.testfaelle.Testfall02_FeutzYvonne;
-import ch.dvbern.ebegu.tets.TestDataUtil;
+import java.awt.Desktop;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.inject.Inject;
+
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.arquillian.persistence.UsingDataSet;
@@ -19,9 +18,15 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import javax.inject.Inject;
-import java.util.ArrayList;
-import java.util.List;
+import ch.dvbern.ebegu.entities.Gesuch;
+import ch.dvbern.ebegu.entities.InstitutionStammdaten;
+import ch.dvbern.ebegu.rechner.AbstractBGRechnerTest;
+import ch.dvbern.ebegu.rules.BetreuungsgutscheinEvaluator;
+import ch.dvbern.ebegu.services.GesuchService;
+import ch.dvbern.ebegu.services.PrintBegleitschreibenPDFService;
+import ch.dvbern.ebegu.testfaelle.Testfall01_WaeltiDagmar;
+import ch.dvbern.ebegu.testfaelle.Testfall02_FeutzYvonne;
+import ch.dvbern.ebegu.tets.TestDataUtil;
 
 /**
  * Test der die vom JA gemeldeten Testfaelle ueberprueft.
@@ -69,6 +74,7 @@ public class PrintBegleitschreibenPDFServiceBeanTest extends AbstractEbeguTest {
 		byte[] bytes = printBegleitschreibenPDFService.printBegleitschreiben(gesuch);
 
 		writeToTempDir(bytes, "BegleitschreibenWaelti.pdf");
+		// openPDF(file);
 
 	}
 
@@ -77,17 +83,28 @@ public class PrintBegleitschreibenPDFServiceBeanTest extends AbstractEbeguTest {
 
 		List<InstitutionStammdaten> institutionStammdatenList = new ArrayList<>();
 		institutionStammdatenList.add(TestDataUtil.createInstitutionStammdatenKitaAaregg());
-		institutionStammdatenList.add(TestDataUtil.createInstitutionStammdatenKitaBruennen()); //sollte tagi aareg sein ist aber hier egal
+		institutionStammdatenList.add(TestDataUtil.createInstitutionStammdatenKitaBruennen()); // sollte tagi aareg sein
+																								// ist aber hier egal
 
 		Testfall02_FeutzYvonne testfall = new Testfall02_FeutzYvonne(TestDataUtil.createGesuchsperiode1617(), institutionStammdatenList);
 		Gesuch gesuch = testfall.createGesuch();
 		TestDataUtil.calculateFinanzDaten(gesuch);
 		gesuch.setGesuchsperiode(TestDataUtil.createGesuchsperiode1617());
-//		evaluator.evaluate(gesuch, AbstractBGRechnerTest.getParameter());
+		// evaluator.evaluate(gesuch, AbstractBGRechnerTest.getParameter());
 
 		byte[] bytes = printBegleitschreibenPDFService.printBegleitschreiben(gesuch);
 
-		writeToTempDir(bytes, "BegleitschreibenFeutz.pdf");
+		File file = writeToTempDir(bytes, "BegleitschreibenFeutz.pdf");
+		// openPDF(file);
 
+	}
+
+	private void openPDF(File file) {
+
+		try {
+			Desktop.getDesktop().open(file);
+		} catch (IOException ex) {
+			// no application registered for PDFs
+		}
 	}
 }
