256976e1c7b3591947ec93726b60c67e7145a616
Imanol Bernabeu <imanol.bernabeu@dvbern.ch>
2016-09-20 16:28:16 +0200

    EBEGU-314 - Fehlende EbeguVorlageKey hinzugefuegt

--- ebegu-shared/src/main/java/ch/dvbern/ebegu/enums/EbeguVorlageKey.java
+++ ebegu-shared/src/main/java/ch/dvbern/ebegu/enums/EbeguVorlageKey.java
@@ -11,13 +11,19 @@ public enum EbeguVorlageKey {
 	// Die zweite Mahnung, falls das Gesuch unvollständig eingereicht wurde
 	VORLAGE_MAHNUNG_2,
 
+	// Verfügung des Angebots KITA
+	VORLAGE_VERFUEGUNG_KITA,
+
 	// Eine Angebotsübergreifende Verfügung Verfügung Kita Verfügung des Angebots Kita
 	VORLAGE_NICHT_EINTRETENSVERFUEGUNG,
 
 	// Verfügung des Angebots Tageseltern Kleinkinder
 	VORLAGE_VERFUEGUNG_TAGESELTERN_KLEINKINDER,
 
-	// Voll-Verfügung (Brief) des Angebots Tageseltern Schulkinder
+	// Verfügung (Brief) des Angebots Tagesstaette Schulkinder
+	VORLAGE_BRIEF_TAGESSTAETTE_SCHULKINDER,
+
+	// Verfügung (Brief) des Angebots Tageseltern Schulkinder
 	VORLAGE_BRIEF_TAGESELTERN_SCHULKINDER,
 
 	// Die Freigabequittung die der Gesuchsteller ausdruckt und ans Jugendamt schickt. Die Freigabequittung geht ans
