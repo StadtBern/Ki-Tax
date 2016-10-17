bd0e4e3f2e9f2f8f3895cf83dcceec40b76998c2
Ramon Marti <ramon.marti@dvbern.ch>
2016-10-17 10:39:52 +0200

    Delay Progressbar hide()
    Interface für den Controller wurde ebenfalls entfernt, da unnötig.

--- ebegu-web/src/core/directive/dv-loading/dv-loading.ts
+++ ebegu-web/src/core/directive/dv-loading/dv-loading.ts
@@ -1,22 +1,21 @@
 import {IDirective, IDirectiveFactory, IHttpService} from 'angular';
 import Moment = moment.Moment;
-
-
-interface IDVLoadingController {
-    isLoading: () => {};
-}
+import ITimeoutService = angular.ITimeoutService;
 
 export class DVLoading implements IDirective {
     restrict = 'A';
     controller = DVLoadingController;
     controllerAs = 'vm';
 
-    link = (scope: ng.IScope, element: ng.IAugmentedJQuery, attributes: ng.IAttributes, controller: IDVLoadingController) => {
+    link = (scope: ng.IScope, element: ng.IAugmentedJQuery, attributes: ng.IAttributes, controller: DVLoadingController) => {
         scope.$watch(controller.isLoading, function (v) {
             if (v) {
                 element.show();
             } else {
-                element.hide();
+                controller.$timeout(function () {
+                    element.hide()
+                }, 500);
+
             }
         });
     };
@@ -31,14 +30,14 @@ export class DVLoading implements IDirective {
 /**
  * Direktive  die ein Element ein oder ausblendet jenachdem ob ein http request pending ist
  */
-export class DVLoadingController implements IDVLoadingController {
+export class DVLoadingController {
 
-    static $inject: string[] = ['$http'];
+    static $inject: string[] = ['$http', '$timeout'];
 
     isLoading: () => {};
 
     /* @ngInject */
-    constructor(private $http: IHttpService) {
+    constructor(private $http: IHttpService, public $timeout: ITimeoutService) {
         this.isLoading = (): boolean => {
             return this.$http.pendingRequests.length > 0;
         };
