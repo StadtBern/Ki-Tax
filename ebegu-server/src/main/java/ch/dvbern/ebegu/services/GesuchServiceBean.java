f4841280b7868ca63356d0476818a1ab568464fd
Imanol Bernabeu <imanol.bernabeu@dvbern.ch>
2016-10-21 11:06:32 +0200

    EBEGU-540 - benutzer in verantwortlicher umbenannt. Man hat zwischen user und benutzer nicht unterscheiden koennen und daher kam der Fehler

--- ebegu-server/src/main/java/ch/dvbern/ebegu/services/GesuchServiceBean.java
+++ ebegu-server/src/main/java/ch/dvbern/ebegu/services/GesuchServiceBean.java
@@ -165,7 +165,7 @@ public class GesuchServiceBean extends AbstractBaseService implements GesuchServ
 		Root<Gesuch> root = query.from(Gesuch.class);
 		// Join all the relevant relations
 		Join<Gesuch, Fall> fall = root.join(Gesuch_.fall, JoinType.INNER);
-		Join<Fall, Benutzer> benutzer = fall.join(Fall_.verantwortlicher, JoinType.LEFT);
+		Join<Fall, Benutzer> verantwortlicher = fall.join(Fall_.verantwortlicher, JoinType.LEFT);
 		Join<Gesuch, Gesuchsperiode> gesuchsperiode = root.join(Gesuch_.gesuchsperiode, JoinType.INNER);
 		Join<Gesuch, Gesuchsteller> gesuchsteller1 = root.join(Gesuch_.gesuchsteller1, JoinType.LEFT);
 		Join<Gesuch, Gesuchsteller> gesuchsteller2 = root.join(Gesuch_.gesuchsteller2, JoinType.LEFT);
@@ -200,10 +200,10 @@ public class GesuchServiceBean extends AbstractBaseService implements GesuchServ
 				predicates.add(predicateRichtigerAngebotstypOderNichtAusgefuellt);
 				break;
 			case SACHBEARBEITER_TRAEGERSCHAFT:
-				predicates.add(cb.equal( institution.get(Institution_.traegerschaft), user.getTraegerschaft()));
+				predicates.add(cb.equal(institution.get(Institution_.traegerschaft), user.getTraegerschaft()));
 				break;
 			case SACHBEARBEITER_INSTITUTION:
-				// es geht hier nicht um die institutin des zugewiesenen benutzers sondern um die institution des eingeloggten benutzers
+				// es geht hier nicht um die institution des zugewiesenen benutzers sondern um die institution des eingeloggten benutzers
 				predicates.add(cb.equal(institution, user.getInstitution()));
 				break;
 			case SCHULAMT:
@@ -258,8 +258,8 @@ public class GesuchServiceBean extends AbstractBaseService implements GesuchServ
 				String[] strings = predicateObjectDto.getVerantwortlicher().split(" ");
 				predicates.add(
 					cb.and(
-						cb.equal(benutzer.get(Benutzer_.vorname), strings[0]),
-						cb.equal(benutzer.get(Benutzer_.nachname), strings[1])
+						cb.equal(verantwortlicher.get(Benutzer_.vorname), strings[0]),
+						cb.equal(verantwortlicher.get(Benutzer_.nachname), strings[1])
 					));
 			}
 		}
