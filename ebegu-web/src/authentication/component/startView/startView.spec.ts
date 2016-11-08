4a893a15276e5e8adf155ca722c45bb729b65657
Markus Hofstetter <markus.hofstetter@dvbern.ch>
2016-11-08 12:38:43 +0100

    EBEGU-582 Startpage für login über IAM erstellen
    > test improved

--- ebegu-web/src/authentication/component/startView/startView.spec.ts
+++ ebegu-web/src/authentication/component/startView/startView.spec.ts
@@ -4,6 +4,10 @@ import {EbeguAuthentication} from '../../authentication.module';
 import {TSAuthEvent} from '../../../models/enums/TSAuthEvent';
 import {StartViewController} from './startView';
 import {EbeguWebCore} from '../../../core/core.module';
+import AuthServiceRS from '../../service/AuthServiceRS.rest';
+import TSUser from '../../../models/TSUser';
+import {TSRole} from '../../../models/enums/TSRole';
+import {IStateService} from 'angular-ui-router';
 import IRootScopeService = angular.IRootScopeService;
 import IScope = angular.IScope;
 
@@ -17,13 +21,25 @@ describe('startView', function () {
     let scope: IScope;
     let $componentController: any;
     let startViewController: StartViewController;
+    let authService: AuthServiceRS;
+    let mockPrincipal: TSUser;
+    let state: IStateService;
 
     beforeEach(angular.mock.inject(function ($injector: any) {
         $componentController = $injector.get('$componentController');
         $rootScope = $injector.get('$rootScope');
         scope = $rootScope.$new();
-        startViewController = new StartViewController($injector.get('$state'), $rootScope, $injector.get('AuthServiceRS'));
+        authService = $injector.get('AuthServiceRS');
+        state = $injector.get('$state');
+        startViewController = new StartViewController(state, $rootScope, authService);
+
     }));
+    beforeEach(function () {
+        mockPrincipal = new TSUser();
+        mockPrincipal.nachname = 'mockprincipal';
+        mockPrincipal.vorname = 'tester';
+        mockPrincipal.role = TSRole.GESUCHSTELLER;
+    });
 
     it('should be defined', function () {
         /*
@@ -34,8 +50,24 @@ describe('startView', function () {
     });
 
     it('should  broadcast "AUTH_EVENTS.notAuthenticated" if no principal is available', function () {
-        let broadcast =  spyOn($rootScope, '$broadcast');
+        let broadcast = spyOn($rootScope, '$broadcast');
         startViewController.$onInit();
         expect(broadcast).toHaveBeenCalledWith(TSAuthEvent.NOT_AUTHENTICATED, 'not logged in on startpage');
+    });
+
+    describe('should  redirect based on role of principal', function () {
+        it('should go to gesuchstellerDashboard if role is gesuchsteller', function () {
+            spyOn(authService, 'getPrincipal').and.returnValue(mockPrincipal);
+            spyOn(state, 'go');
+            startViewController.$onInit();
+            expect(state.go).toHaveBeenCalledWith('gesuchstellerDashboard');
+        });
+        it('should go to pendenzen if role is sachbearbeiter ja', function () {
+            mockPrincipal.role = TSRole.SACHBEARBEITER_JA;
+               spyOn(authService, 'getPrincipal').and.returnValue(mockPrincipal);
+               spyOn(state, 'go');
+               startViewController.$onInit();
+               expect(state.go).toHaveBeenCalledWith('pendenzen');
+           });
     });
 });
