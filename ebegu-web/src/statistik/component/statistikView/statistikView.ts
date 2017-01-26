5901fce5850e8cf670c74fc57ceba595a7899332
Ramon Marti <ramon.marti@dvbern.ch>
2017-01-26 10:55:16 +0100

    EBEGU-404: Frontend "Statisitiken" inklusive Sichtbarkeit nach Rollen

--- ebegu-web/src/statistik/component/statistikView/statistikView.ts
+++ ebegu-web/src/statistik/component/statistikView/statistikView.ts
@@ -15,6 +15,7 @@ export class StatistikViewComponentConfig implements IComponentOptions {
 export class StatistikViewController {
 
     static $inject: string[] = ['EbeguUtil', '$state'];
+    accordion: string = '';
 
     constructor(private ebeguUtil: EbeguUtil, private $state: IStateService) {
         this.initViewModel();
@@ -22,4 +23,25 @@ export class StatistikViewController {
 
     private initViewModel() {
     }
+   /* public toggleAccordion( i: string ): void {
+        if (this.isAccordionOpen(i)) {
+            this.accordion.splice(this.accordion.indexOf(i), 1);
+        } else {
+            this.accordion = [];
+            this.accordion.push(i);
+        }
+    }
+    public isAccordionOpen( i: string ): boolean {
+        return this.accordion.indexOf(i) > -1;
+    }*/
+     public toggleAccordion( i: string ): void {
+     if (this.isAccordionOpen(i)) {
+     this.accordion = '';
+     } else {
+     this.accordion = i;
+     }
+     }
+     public isAccordionOpen( i: string ): boolean {
+     return this.accordion === i;
+     }
 }
