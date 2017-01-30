3339370d501b84d8bcbec5955b00be9368d42cbd
HEFR <franziska.herger@dvbern.ch>
2017-01-30 14:17:20 +0100

    EBEGU-42: Mitteilung hat neu Verknüpfung zu Betreuung

--- ebegu-rest/src/main/java/ch/dvbern/ebegu/api/converter/JaxBConverter.java
+++ ebegu-rest/src/main/java/ch/dvbern/ebegu/api/converter/JaxBConverter.java
@@ -2188,6 +2188,9 @@ public class JaxBConverter {
 		} else {
 			throw new EbeguEntityNotFoundException("mitteilungToEntity", ErrorCodeEnum.ERROR_ENTITY_NOT_FOUND, mitteilungJAXP.getFall());
 		}
+		if (mitteilungJAXP.getBetreuung() != null) {
+			mitteilung.setBetreuung(betreuungToEntity(mitteilungJAXP.getBetreuung(), new Betreuung()));
+		}
 		mitteilung.setMessage(mitteilungJAXP.getMessage());
 		mitteilung.setMitteilungStatus(mitteilungJAXP.getMitteilungStatus());
 
@@ -2216,6 +2219,9 @@ public class JaxBConverter {
 		jaxMitteilung.setEmpfaengerTyp(persistedMitteilung.getEmpfaengerTyp());
 		if (persistedMitteilung.getFall() != null) {
 			jaxMitteilung.setFall(fallToJAX(persistedMitteilung.getFall()));
+		}
+		if (persistedMitteilung.getBetreuung() != null) {
+			jaxMitteilung.setBetreuung(betreuungToJAX(persistedMitteilung.getBetreuung()));
 		}
 		jaxMitteilung.setMessage(persistedMitteilung.getMessage());
 		jaxMitteilung.setMitteilungStatus(persistedMitteilung.getMitteilungStatus());
--- ebegu-web/src/utils/EbeguRestUtil.ts
+++ ebegu-web/src/utils/EbeguRestUtil.ts
@@ -1828,6 +1828,7 @@ export default class EbeguRestUtil {
         if (mitteilungFromServer) {
             this.parseAbstractEntity(tsMitteilung, mitteilungFromServer);
             tsMitteilung.fall = this.parseFall(new TSFall(), mitteilungFromServer.fall);
+            tsMitteilung.betreuung = this.parseBetreuung(new TSBetreuung(), mitteilungFromServer.betreuung);
             tsMitteilung.senderTyp = mitteilungFromServer.senderTyp;
             tsMitteilung.empfaengerTyp = mitteilungFromServer.empfaengerTyp;
             tsMitteilung.sender = this.parseUser(new TSUser(), mitteilungFromServer.sender);
@@ -1845,6 +1846,7 @@ export default class EbeguRestUtil {
         if (tsMitteilung) {
             this.abstractEntityToRestObject(restMitteilung, tsMitteilung);
             restMitteilung.fall = this.fallToRestObject({}, tsMitteilung.fall);
+            restMitteilung.betreuung = this.betreuungToRestObject({}, tsMitteilung.betreuung);
             restMitteilung.senderTyp = tsMitteilung.senderTyp;
             restMitteilung.empfaengerTyp = tsMitteilung.empfaengerTyp;
             restMitteilung.sender = this.userToRestObject({}, tsMitteilung.sender);
