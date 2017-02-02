3a8483de08a67206ced1356a1e917f76dc0bbff0
Ramon Marti <ramon.marti@dvbern.ch>
2017-02-02 19:05:39 +0100

    EBEGU-404: Frontend "Statisitiken" inklusive Sichtbarkeit nach Rollen
    'Zwischenspeichern'

--- ebegu-web/src/core/component/dv-accordion/dv-accordion-tab/dv-accordion-tab.ts
+++ ebegu-web/src/core/component/dv-accordion/dv-accordion-tab/dv-accordion-tab.ts
@@ -3,21 +3,21 @@ import {DvAccordionController} from '../dv-accordion';
 let template = require('./dv-accordion-tab.html');
 
 export class DvAccordionTabComponentConfig implements IComponentOptions {
-    transclude = true;
+    transclude: any = {
+        title: '?tabTitle',
+        body: '?tabBody'
+    };
     template = template;
     controller = DvAccordionTabController;
     controllerAs = 'vmt';
     bindings: any = {
-        title: '@',
         tabid: '@',
-        active: '='
     };
     require: any = {vma: '^dvAccordion'};
 
 }
 
 export class DvAccordionTabController {
-    title: string;
     tabid: string;
     vma: DvAccordionController;
     static $inject: any[] = [];
--- ebegu-web/src/statistik/component/statistikView/statistikView.ts
+++ ebegu-web/src/statistik/component/statistikView/statistikView.ts
@@ -1,6 +1,10 @@
 import {IComponentOptions} from 'angular';
 import EbeguUtil from '../../../utils/EbeguUtil';
 import {IStateService} from 'angular-ui-router';
+import TSStatistikParameter from '../../../models/TSStatistikParameter';
+import {TSStatistikParameterType} from '../../../models/enums/TSStatistikParameterType';
+import TSGesuchsperiode from '../../../models/TSGesuchsperiode';
+import GesuchsperiodeRS from '../../../core/service/gesuchsperiodeRS.rest';
 
 let template = require('./statistikView.html');
 require('./statistikView.less');
@@ -13,13 +17,50 @@ export class StatistikViewComponentConfig implements IComponentOptions {
 }
 
 export class StatistikViewController {
+    private _statistikParameter: TSStatistikParameter;
+    private _gesuchsperioden: Array<TSGesuchsperiode>;
 
-    static $inject: string[] = ['EbeguUtil', '$state'];
 
-    constructor(private ebeguUtil: EbeguUtil, private $state: IStateService) {
+    static $inject: string[] = ['EbeguUtil', '$state', 'GesuchsperiodeRS'];
+
+    constructor(private ebeguUtil: EbeguUtil, private $state: IStateService, private gesuchsperiodeRS: GesuchsperiodeRS) {
+        this._statistikParameter = new TSStatistikParameter();
         this.initViewModel();
+        this.gesuchsperiodeRS.getAllGesuchsperioden().then((response: any) => {
+            this._gesuchsperioden = response;
+        });
     }
 
     private initViewModel() {
+    }
+
+    public generateStatistik(type?: TSStatistikParameterType): TSStatistikParameter {
+        this._statistikParameter.type = (<any>TSStatistikParameterType)[type];
+        let tmpParameter = angular.copy(this._statistikParameter);
+        if (this._statistikParameter.type === TSStatistikParameterType.GESUCHSTELLER ||
+            this._statistikParameter.type === TSStatistikParameterType.KANTON
+            ) {
+            tmpParameter.gesuchsperiode = null;
+        }
+        if (this._statistikParameter.type === TSStatistikParameterType.GESUCH_STICHTAG ||
+            this._statistikParameter.type === TSStatistikParameterType.GESUCHSTELLER
+            ) {
+            tmpParameter.von = null;
+            tmpParameter.bis = null;
+        }
+        if (this._statistikParameter.type !== TSStatistikParameterType.GESUCH_STICHTAG &&
+            this._statistikParameter.type !== TSStatistikParameterType.GESUCHSTELLER
+            ) {
+            tmpParameter.stichtag = null;
+        }
+        return tmpParameter;
+    }
+
+    get statistikParameter(): TSStatistikParameter {
+        return this._statistikParameter;
+    }
+
+    get gesuchsperioden(): Array<TSGesuchsperiode> {
+        return this._gesuchsperioden;
     }
 }
