137b57ee2f3374f02f4f749689701472c24f62e1
Ramon Marti <ramon.marti@dvbern.ch>
2017-03-02 15:58:29 +0100

    EBEGU-34 Warnung vor Zeitüberschreitung beim Ausfüllen: Kunden wird Countdownuhr angezeigt, die 10 Minuten lang sekundenweise zurückzählt und jedes Mal wieder bei 10 Min startet, wenn Kunde Daten gesendet hat

--- ebegu-web/src/core/component/dv-countdown/dv-countdown.ts
+++ ebegu-web/src/core/component/dv-countdown/dv-countdown.ts
@@ -1,9 +1,16 @@
 import {IComponentOptions, IIntervalService} from 'angular';
 import {TSRoleUtil} from '../../../utils/TSRoleUtil';
 import {IStateService} from 'angular-ui-router';
+import {TSHTTPEvent} from '../../events/TSHTTPEvent';
+import {DvDialog} from '../../directive/dv-dialog/dv-dialog';
+import {OkDialogController} from '../../../gesuch/dialog/OkDialogController';
+import AuthServiceRS from '../../../authentication/service/AuthServiceRS.rest';
+import {TSRole} from '../../../models/enums/TSRole';
 import IPromise = angular.IPromise;
 import Moment = moment.Moment;
+import IRootScopeService = angular.IRootScopeService;
 let template = require('./dv-countdown.html');
+let dialogTemplate = require('../../../gesuch/dialog/okDialogTemplate.html');
 
 export class DvCountdownComponentConfig implements IComponentOptions {
     transclude = false;
@@ -17,42 +24,61 @@ export class DvCountdownController {
 
     TSRoleUtil: any;
     timer: moment.Duration;
-    timerPromise: IPromise<any>;
+    timerInterval: IPromise<any>;
 
-    static $inject: any[] = ['$state', '$interval'];
+    static $inject: any[] = ['AuthServiceRS', '$state', '$interval', '$rootScope', 'DvDialog'];
 
-    constructor(private $state: IStateService, private $interval: IIntervalService) {
+    constructor(private authServiceRS: AuthServiceRS, private $state: IStateService, private $interval: IIntervalService, private $rootScope: IRootScopeService, private DvDialog: DvDialog) {
         this.TSRoleUtil = TSRoleUtil;
-        this.startTimer();
+        this.$rootScope.$on(TSHTTPEvent[TSHTTPEvent.REQUEST_FINISHED], () => {
+            if (this.authServiceRS.isRole(TSRole.GESUCHSTELLER)) {
+                if (this.timerInterval === undefined) {
+                    this.startTimer();
+                } else {
+                    this.resetTimer();
+                }
+            } else {
+                    this.cancelInterval();
+            }
+        });
+
     }
-    public goBackHome(): void {
-        this.$state.go('gesuchstellerDashboard');
-    }
+
     public getTimeLeft(): string {
-        if (this.timer.asMinutes() < 5) {
-            return this.timer.minutes() + ' : ' +  (this.timer.seconds() < 10 ? '0' + this.timer.seconds() : this.timer.seconds());
-        } else {
-            return '';
+        if (this.timer) {
+            if (this.timer.asMinutes() < 5) {
+                return this.timer.minutes() + ' : ' + (this.timer.seconds() < 10 ? '0' + this.timer.seconds() : this.timer.seconds());
+            }
         }
+        return '';
     }
 
     public decrease() {
         this.timer.asMilliseconds() <= 0 ? this.stopTimer() : this.timer = moment.duration(this.timer.asSeconds() - 1, 'seconds');
     }
-    public stopTimer(): void {
-        if (this.timerPromise !== undefined) {
-            this.$interval.cancel(this.timerPromise);
-            this.timerPromise = undefined;
-        } else {
-            this.startTimer();
-        }
-    }
-    public startTimer(): void {
-        this.timer = moment.duration(5, 'minutes');
-        this.timerPromise = this.$interval(this.decrease.bind(this), 10);
+    //Fuer Testzwecke hier auf 5 setzen, ab dann erscheint der Countdown naemlich
+    public resetTimer(): void {
+        this.timer = moment.duration(10, 'minutes');
     }
 
-    public isCurrentPageGSDashboard(): boolean {
-        return (this.$state.current && this.$state.current.name === 'gesuchstellerDashboard');
+    public stopTimer(): void {
+        this.cancelInterval();
+        this.DvDialog.showDialog(dialogTemplate, OkDialogController, {
+            title: 'Bitte fahren Sie mit der Bearbeitung fort',
+        });
+    }
+
+    public cancelInterval(): void {
+        if (this.timerInterval !== undefined) {
+            this.$interval.cancel(this.timerInterval);
+            this.timerInterval = undefined;
+            this.timer = undefined;
+        }
+    }
+
+    public startTimer(): void {
+        this.resetTimer();
+        //Fuer Testzwecke hier auf 10 oder 100 setzen
+        this.timerInterval = this.$interval(this.decrease.bind(this), 1000);
     }
 }
