fe2d5930f8bf0e3b1cbed88aea58c4c1bc411376
Imanol Bernabeu <imanol.bernabeu@dvbern.ch>
2017-06-12 12:56:15 +0200

    EBEGU-1103 - nicht benutzte Variable geloescht

--- ebegu-web/src/core/component/dv-antrag-list/dv-antrag-list.ts
+++ ebegu-web/src/core/component/dv-antrag-list/dv-antrag-list.ts
@@ -58,7 +58,6 @@ export class DVAntragListController {
     selectedFamilienName: string;
     selectedKinder: string;
     selectedAenderungsdatum: string;
-    selectedEingangsdatum: string;
     selectedEingangsdatumSTV: string;
     selectedVerantwortlicher: TSUser;
 
