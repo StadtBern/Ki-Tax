2571fdd932eef5d1a70e99e25aae72da15caaa7b
HEFR <franziska.herger@dvbern.ch>
2017-10-03 13:41:30 +0200

    EBEGU-1338: Lizenz-Header einfügen

--- ebegu-shared/src/main/java/ch/dvbern/ebegu/authentication/PrincipalBean.java
+++ ebegu-shared/src/main/java/ch/dvbern/ebegu/authentication/PrincipalBean.java
@@ -1,11 +1,24 @@
+/*
+ * Ki-Tax: System for the management of external childcare subsidies
+ * Copyright (C) 2017 City of Bern Switzerland
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package ch.dvbern.ebegu.authentication;
 
-import ch.dvbern.ebegu.entities.Benutzer;
-import ch.dvbern.ebegu.entities.Mandant;
-import ch.dvbern.ebegu.enums.UserRole;
-import ch.dvbern.ebegu.services.BenutzerService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import java.security.Principal;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
@@ -14,10 +27,13 @@ import javax.ejb.SessionContext;
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
 import javax.transaction.TransactionSynchronizationRegistry;
-import java.security.Principal;
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
+
+import ch.dvbern.ebegu.entities.Benutzer;
+import ch.dvbern.ebegu.entities.Mandant;
+import ch.dvbern.ebegu.enums.UserRole;
+import ch.dvbern.ebegu.services.BenutzerService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import static com.google.common.base.Preconditions.checkNotNull;
 
