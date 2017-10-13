628fb914ae1bee8fde716d1c09a6fc27e7a60b19
HEFR <franziska.herger@dvbern.ch>
2017-10-13 13:56:01 +0200

    EBEGU-1438: Vorschlag für die Schnittstelle mit Scolaris

--- ebegu-rest/src/main/java/ch/dvbern/ebegu/api/resource/schulamt/SchulamtBackendResource.java
+++ ebegu-rest/src/main/java/ch/dvbern/ebegu/api/resource/schulamt/SchulamtBackendResource.java
@@ -26,6 +26,7 @@ import javax.inject.Inject;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -88,7 +89,7 @@ public class SchulamtBackendResource {
 	@Consumes(MediaType.WILDCARD)
 	@Produces(MediaType.APPLICATION_JSON)
 	@Path("/anmeldung/{bgNummer}")
-	public Response getAnmeldung(@Nonnull String bgNummer) {
+	public Response getAnmeldung(@Nonnull @PathParam("bgNummer") String bgNummer) {
 		Validate.notNull(bgNummer);
 
 		JaxExternalAnmeldung anmeldung;
@@ -106,7 +107,7 @@ public class SchulamtBackendResource {
 	@Consumes(MediaType.WILDCARD)
 	@Produces(MediaType.APPLICATION_JSON)
 	@Path("/anmeldungTagesschule/{bgNummer}")
-	public Response getAnmeldungTagesschule(@Nonnull String bgNummer) {
+	public Response getAnmeldungTagesschule(@Nonnull @PathParam("bgNummer") String bgNummer) {
 		Validate.notNull(bgNummer);
 
 		if (!bgNummer.endsWith("1")) {
@@ -128,7 +129,7 @@ public class SchulamtBackendResource {
 	@Consumes(MediaType.WILDCARD)
 	@Produces(MediaType.APPLICATION_JSON)
 	@Path("/anmeldungFerieninsel/{bgNummer}")
-	public Response getAnmeldungFerieninsel(@Nonnull String bgNummer) {
+	public Response getAnmeldungFerieninsel(@Nonnull @PathParam("bgNummer") String bgNummer) {
 		Validate.notNull(bgNummer);
 
 		if (bgNummer.endsWith("1")) {
