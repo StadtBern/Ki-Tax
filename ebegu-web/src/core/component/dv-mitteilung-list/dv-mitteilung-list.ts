15968084172858ce20bd28912c52aed5d90a7491
Ramon Marti <ramon.marti@dvbern.ch>
2018-02-08 16:46:27 +0100

    EBEGU-1701 UX Posteingang: Darstellung Meldung-Übergeben / Verantwortlichkeit des Amtes

--- ebegu-web/src/core/component/dv-mitteilung-list/dv-mitteilung-list.ts
+++ ebegu-web/src/core/component/dv-mitteilung-list/dv-mitteilung-list.ts
@@ -254,6 +254,10 @@ export class DVMitteilungListController {
         return mitteilung && mitteilung.sender && mitteilung.senderTyp === TSMitteilungTeilnehmerTyp.INSTITUTION;
     }
 
+    public isSenderTypSchulamt(mitteilung: TSMitteilung): boolean {
+        return mitteilung && mitteilung.sender && mitteilung.senderTyp === TSMitteilungTeilnehmerTyp.JUGENDAMT && mitteilung.getEmpfaengerAmt() === TSAmt.SCHULAMT;
+    }
+
     public isSenderTypJugendamt(mitteilung: TSMitteilung): boolean {
         return mitteilung && mitteilung.sender && mitteilung.senderTyp === TSMitteilungTeilnehmerTyp.JUGENDAMT;
     }
@@ -396,11 +400,11 @@ export class DVMitteilungListController {
     }
 
     public canUebergebenAnSchulamt(mitteilung: TSMitteilung): boolean {
-        return this.isUserAndEmpfaengerSameAmt(mitteilung, TSAmt.JUGENDAMT);
+        return this.isUserAndEmpfaengerSameAmt(mitteilung, TSAmt.JUGENDAMT) && !mitteilung.isErledigt();
     }
 
     public canUebergebenAnJugendamt(mitteilung: TSMitteilung): boolean {
-        return this.isUserAndEmpfaengerSameAmt(mitteilung, TSAmt.SCHULAMT);
+        return this.isUserAndEmpfaengerSameAmt(mitteilung, TSAmt.SCHULAMT) && !mitteilung.isErledigt();
     }
 
     private isUserAndEmpfaengerSameAmt(mitteilung: TSMitteilung, amt: TSAmt): boolean {
