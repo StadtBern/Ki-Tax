b8572e40520545b7a7f09ccf1c69c6e5e0353917
HEFR <franziska.herger@dvbern.ch>
2018-02-22 13:23:23 +0100

    EBEGU-1702: Papieranmeldung ohne Detailinformationen. Tests angepasst

--- ebegu-dbschema/src/main/resources/db/migration/V0072__anmeldungOhneDetail.sql
+++ ebegu-dbschema/src/main/resources/db/migration/V0072__anmeldungOhneDetail.sql
@@ -1,4 +1,4 @@
 ALTER TABLE betreuung ADD keine_detailinformationen BIT;
 ALTER TABLE betreuung_aud ADD keine_detailinformationen BIT;
 
-update betreuung set betreuung.keine_detailinformationen = false;
+update betreuung set keine_detailinformationen = false;
