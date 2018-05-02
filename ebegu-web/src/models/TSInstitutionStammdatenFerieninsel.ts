95ec7f30ad99c5619acbcdd0459980bb0f605897
Imanol Bernabeu <imanol.bernabeu@dvbern.ch>
2018-05-02 16:55:58 +0200

    EBEGU-1875 - Bei Feriensequenzen bei denen es einen ausweichstandort definiert wurde, bekommt der Benutzer eine Infomeldung wenn er die Feriensequenz fuer eine Anmeldung auswaehlt

--- ebegu-web/src/models/TSInstitutionStammdatenFerieninsel.ts
+++ ebegu-web/src/models/TSInstitutionStammdatenFerieninsel.ts
@@ -13,6 +13,8 @@
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
+import EbeguUtil from '../utils/EbeguUtil';
+import {TSFerienname} from './enums/TSFerienname';
 import TSAbstractEntity from './TSAbstractEntity';
 
 export default class TSInstitutionStammdatenFerieninsel extends TSAbstractEntity {
@@ -56,5 +58,35 @@ export default class TSInstitutionStammdatenFerieninsel extends TSAbstractEntity
 
     set ausweichstandortFruehlingsferien(value: string) {
         this._ausweichstandortFruehlingsferien = value;
+    }
+
+    public isDefined(ferienname: TSFerienname): boolean {
+        switch (ferienname) {
+            case TSFerienname.FRUEHLINGSFERIEN:
+                return !EbeguUtil.isEmptyStringNullOrUndefined(this.ausweichstandortFruehlingsferien);
+            case TSFerienname.HERBSTFERIEN:
+                return !EbeguUtil.isEmptyStringNullOrUndefined(this.ausweichstandortHerbstferien);
+            case TSFerienname.SOMMERFERIEN:
+                return !EbeguUtil.isEmptyStringNullOrUndefined(this.ausweichstandortSommerferien);
+            case TSFerienname.SPORTFERIEN:
+                return !EbeguUtil.isEmptyStringNullOrUndefined(this.ausweichstandortSportferien);
+            default:
+                return false;
+        }
+    }
+
+    public getAusweichstandortFromFerienname(ferienname: TSFerienname): string {
+        switch (ferienname) {
+            case TSFerienname.FRUEHLINGSFERIEN:
+                return this.ausweichstandortFruehlingsferien;
+            case TSFerienname.HERBSTFERIEN:
+                return this.ausweichstandortHerbstferien;
+            case TSFerienname.SOMMERFERIEN:
+                return this.ausweichstandortSommerferien;
+            case TSFerienname.SPORTFERIEN:
+                return this.ausweichstandortSportferien;
+            default:
+                return '';
+        }
     }
 }
