8e504a35b6b60e3debeefca948d00513e972251f
HEFR <franziska.herger@dvbern.ch>
2018-05-25 09:39:38 +0200

    EBEGU-1897: Diverse Security-Header

--- ebegu-web/config/webpack.dev.ts
+++ ebegu-web/config/webpack.dev.ts
@@ -92,8 +92,8 @@ export default (env: any): webpack.Configuration => webpackMerge(commonConfig(en
         },
         headers: {
             'Content-Security-Policy': "default-src 'none'; object-src 'self'; script-src 'self' 'unsafe-eval' ajax.googleapis.com; connect-src 'self' ws:; img-src 'self' data:; style-src 'self' 'unsafe-inline'  https://fonts.googleapis.com; font-src 'self'  https://fonts.googleapis.com https://fonts.gstatic.com;",
-            'X-Frame-Options': 'DENY'
-        }
-
+            'X-Frame-Options': 'DENY',
+            'X-XSS-Protection': '1; mode=block', 'X-Content-Type-Options': 'nosniff', 'Referrer-Policy': 'strict-origin'
+		}
     }
 });
